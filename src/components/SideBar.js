42b0bed35c5e8df67227a7f095751a5d905894bf
MariaLauraElias <eliasmarialaura@gmail.com>
2022-12-01 10:25:47 -0300

    cambios en sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,38 +3,38 @@ import image from '../assets/images/02-Logo-small.jpeg';
 import ContentWrapper from './ContentWrapper';
 import GenresInDb from './GenresInDb';
 import LastMovieInDb from './LastMovieInDb';
-import ContentRowMovies from './ContentRowMovies';
 import NotFound from './NotFound';
 import {Link, Route, Switch} from 'react-router-dom';
 import SearchMovies from './SearchMovies';
+import Chart from './Chart';
 
 function SideBar(){
     return(
         <React.Fragment>
-            {/*<!-- Sidebar -->*/}
+            
             <ul className="navbar-nav bg-gradient-blue sidebar sidebar-ligh accordion" id="accordionSidebar">
 
-                {/*<!-- Sidebar - Brand -->*/}
+                
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
                     <div className="sidebar-brand-icon">
-                        <img className="w-100" src={image} alt="Digital House"/>
+                        <img className="w-100" src={image} alt="Entrelineas"/>
                     </div>
                 </a>
 
-                {/*<!-- Divider -->*/}
+                
                 <hr className="sidebar-divider my-0"/>
 
-                {/*<!-- Nav Item - Dashboard -->*/}
+                
                 <li className="nav-item active">
                     <Link className="nav-link" to="/">
                         <i className="fas fa-fw fa-tachometer-alt"></i>
                         <span>Dashboard - Entrelineas</span></Link>
                 </li>
 
-                {/*<!-- Divider -->*/}
+                
                 <hr className="sidebar-divider"/>
 
-                {/*<!-- Heading -->*/}
+                
                 <div className="sidebar-heading">Acciones</div>
 
                 <li className="nav-item">
@@ -44,7 +44,7 @@ function SideBar(){
                     </Link>
                 </li>
 
-                {/*<!-- Nav Item - Pages -->*/}
+               
                 <li className="nav-item">
                 <Link className="nav-link" to="/GenresInDb">
                         <i className="fas fa-fw fa-folder"></i>
@@ -52,44 +52,30 @@ function SideBar(){
                     </Link>
                 </li>
 
-                {/*<!-- Nav Item - Charts -->*/}
+                
                 <li className="nav-item">
                     <Link className="nav-link" to="/LastMovieInDb">
                         <i className="fas fa-fw fa-chart-area"></i>
                         <span>Último producto </span></Link>
                 </li>
 
-                {/*<!-- Nav Item - Tables -->*/}
+                
                 <li className="nav-item nav-link">
-                <Link className="nav-link" to="/ContentRowMovies">
+                <Link className="nav-link" to="/Chart">
                         <i className="fas fa-fw fa-table"></i>
                         <span>Tabla de productos</span></Link>
                 </li>
 
-                {/*<!-- Divider -->*/}
+                
                 <hr className="sidebar-divider d-none d-md-block"/>
             </ul>
-            {/*<!-- End of Sidebar -->*/}
-
-            {/*<!-- Microdesafio 1 -->*/}
-            {/*<!--<Route exact path="/">
-                <ContentWrapper />
-            </Route>
-            <Route path="/GenresInDb">
-                <GenresInDb />
-            </Route>
-            <Route path="/LastMovieInDb">
-                <LastMovieInDb />
-            </Route>
-            <Route path="/ContentRowMovies">
-                <ContentRowMovies />
-            </Route>*/}
-            {/*<!-- End Microdesafio 1 -->*/}
-
-            {/*<!-- End Microdesafio 2 -->*/}
+            
             <Switch>
                 <Route exact path="/">
                     <ContentWrapper />
+                </Route>
+                <Route path="/SearchMovies">
+                    <SearchMovies />
                 </Route>
                 <Route path="/GenresInDb">
                     <GenresInDb />
@@ -97,15 +83,12 @@ function SideBar(){
                 <Route path="/LastMovieInDb">
                     <LastMovieInDb />
                 </Route>
-                <Route path="/ContentRowMovies">
-                    <ContentRowMovies />
-                </Route>
-                <Route path="/SearchMovies">
-                    <SearchMovies />
+                <Route path="/Chart">
+                    <Chart />
                 </Route>
                 <Route component={NotFound} />
             </Switch>
-            {/*<!-- End Microdesafio 2 -->*/}
+            
         </React.Fragment>
     )
 }
