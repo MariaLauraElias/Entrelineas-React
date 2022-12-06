12f3544e816bbfbe7091ac94f029ebfd13b37ca3
Mateo Herrera Tamayo <teoherrera07@gmail.com>
2022-12-05 19:06:08 -0500

    Actualizo para ultimo usuario

--- src/components/ContentRowCenter.js
+++ src/components/ContentRowCenter.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LastBookInDb from './LastBookInDb';
 import TopicsInDb from './TopicsInDb';
+import LastUserInDb from './LastUserInDb';
 
 function ContentRowCenter(){
     return (
         <div className="row">
             <LastBookInDb />
             <TopicsInDb />
+            <LastUserInDb />
         </div>
     )
 }
